f3961b047a2104ad1ed096a0fb81bc3478995cd1
Greg Guthe <gguthe@mozilla.com>
2018-03-15 12:00:14 -0400

    PS256 support

--- sign_verify.go
+++ sign_verify.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 	"math/big"
 	"crypto"
+	"crypto/rsa"
 	"crypto/ecdsa"
 	"fmt"
 	// "log"
@@ -124,15 +125,27 @@ func getAlg(h *COSEHeaders) (alg *generated.COSEAlgorithm, err error) {
 			}
 
 		}
+	} else if tmp, ok := h.protected[int(1)]; ok {
+		// fmt.Println(fmt.Sprintf("get by value int? %T", tmp))
+		if algValue, ok := tmp.(int); ok {
+			// fmt.Println(fmt.Sprintf("get by value int? %+v", algValue))
+			alg, err = GetAlgByValue(int64(algValue))
+			if err != nil {
+				return nil, err
+			} else {
+				return alg, nil
+			}
+
+		}
 	}
 	// ai, _ := h.protected[uint64(1)].(int)
-	// fmt.Println(fmt.Sprintf("getAlg else %+v %+v", h.protected["alg"], ai))
+	// fmt.Println(fmt.Sprintf("getAlg else %+v %+v", h.protected, ai))
 	return nil, errors.New("Error fetching alg.")
 }
 
 func hashSigStructure(
 	message *COSESignMessage,
-	key *ecdsa.PublicKey,
+	key crypto.PublicKey,
 	external []byte,
 ) (hashed []byte, ToBeSigned []byte, err error) {
 	if message == nil {
@@ -185,6 +198,8 @@ func hashSigStructure(
 		hash = crypto.SHA384
 	} else if alg.Value == GetAlgByNameOrPanic("ES512").Value {
 		hash = crypto.SHA512
+	} else if alg.Value == GetAlgByNameOrPanic("PS256").Value {
+		hash = crypto.SHA256
 	} else {
 		return nil, nil, errors.New("alg not implemented.")
 	}
@@ -196,76 +211,135 @@ func hashSigStructure(
 }
 
 
-// TODO: rewrite as Signer(kid, alg key config).Sign(payload, ext_data)
+// TODO: rewrite as Signer(kid, alg key config).NewSignData(payload, ext_data)
 //
 // Signing and Verification Process
 // https://tools.ietf.org/html/rfc8152#section-4.4
-func Sign(message *COSESignMessage, key *ecdsa.PrivateKey, randReader io.Reader, external []byte) (result *COSESignMessage, err error, ToBeSigned []byte) {
-	hashed, ToBeSigned, err := hashSigStructure(message, &key.PublicKey, external)
-	if err != nil {
-		return nil, err, ToBeSigned
+func Sign(message *COSESignMessage, privateKey crypto.PrivateKey, randReader io.Reader, external []byte) (result *COSESignMessage, err error, ToBeSigned []byte) {
+	// TODO: sign non-first slots
+	// if message.signatures[0].signature != nil || len(message.signatures[0].signature) > 0 {
+	// 	return nil, errors.New(fmt.Sprintf("message already has a signature %s", message.signatures[0].signature)), ToBeSigned
+	// }
+
+	switch key := privateKey.(type) {
+	case *rsa.PrivateKey:
+		hashed, ToBeSigned, err := hashSigStructure(message, &key.PublicKey, external)
+		if err != nil {
+			return nil, err, ToBeSigned
+		}
+
+		// TODO: pass signature alg or pick hash based on protected alg value
+		sig, err := rsa.SignPSS(randReader, key, crypto.SHA256, hashed, nil)
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("rsa SignPSS error %s", err)), ToBeSigned
+		}
+
+		// 4.  Place the resulting signature value in the 'signature' field of the array.
+		message.signatures[0].signature = sig
+
+		return message, nil, ToBeSigned
+	case *ecdsa.PrivateKey:
+		hashed, ToBeSigned, err := hashSigStructure(message, &key.PublicKey, external)
+		if err != nil {
+			return nil, err, ToBeSigned
+		}
+
+		// 3.  Call the signature creation algorithm passing in K (the key to
+		//     sign with), alg (the algorithm to sign with), and ToBeSigned (the
+		//     value to sign).
+
+		// https://tools.ietf.org/html/rfc8152#section-8.1
+		r, s, err := ecdsa.Sign(randReader, key, hashed)
+		if err != nil {
+			return nil, errors.New(fmt.Sprintf("ecdsa.Sign error %s", err)), nil
+		}
+
+		// assert r and s are the same length will be the same length
+		// as the length of the key used for the signature process
+		// fmt.Println(fmt.Printf("\nr: %+v\ns: %+v\n %+x", r, s, hashed))
+
+		// The signature is encoded by converting the integers into
+		// byte strings of the same length as the key size.  The
+		// length is rounded up to the nearest byte and is left padded
+		// with zero bits to get to the correct length.  The two
+		// integers are then concatenated together to form a byte
+		// string that is the resulting signature.
+		curveBits := key.Curve.Params().BitSize
+		keyBytes := curveBits / 8
+		if curveBits % 8 > 0 {
+			keyBytes += 1
+		}
+
+		// fmt.Println(fmt.Printf("\nr: %+x\ns: %+x\n", rBytes, sBytes))
+
+		n := keyBytes
+		sig := make([]byte, 0)
+		sig = append(sig, I2OSP(r, n)...)
+		sig = append(sig, I2OSP(s, n)...)
+
+		// 4.  Place the resulting signature value in the 'signature' field of the array.
+		message.signatures[0].signature = sig
+
+		return message, nil, ToBeSigned
+	default:
+		return nil, errors.New("Unrecognized private key type"), nil
 	}
-
-	// 3.  Call the signature creation algorithm passing in K (the key to
-	//     sign with), alg (the algorithm to sign with), and ToBeSigned (the
-	//     value to sign).
-
-	// https://tools.ietf.org/html/rfc8152#section-8.1
-	r, s, err := ecdsa.Sign(randReader, key, hashed)
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("ecdsa.Sign error %s", err)), nil
-	}
-	// assert r and s are the same length will be the same length
-	// as the length of the key used for the signature process
-	// fmt.Println(fmt.Printf("\nr: %+v\ns: %+v\n %+x", r, s, hashed))
-
-	// The signature is encoded by converting the integers into
-	// byte strings of the same length as the key size.  The
-	// length is rounded up to the nearest byte and is left padded
-	// with zero bits to get to the correct length.  The two
-	// integers are then concatenated together to form a byte
-	// string that is the resulting signature.
-	curveBits := key.Curve.Params().BitSize
-	keyBytes := curveBits / 8
-	if curveBits % 8 > 0 {
-		keyBytes += 1
-	}
-
-	// fmt.Println(fmt.Printf("\nr: %+x\ns: %+x\n", rBytes, sBytes))
-
-	n := keyBytes
-	sig := make([]byte, 0)
-	sig = append(sig, I2OSP(r, n)...)
-	sig = append(sig, I2OSP(s, n)...)
-
-	// 4.  Place the resulting signature value in the 'signature' field of the array.
-	message.signatures[0].signature = sig
-
-	// fmt.Println(fmt.Printf("%+v", message))
-	return message, nil, ToBeSigned
 }
 
-func Verify(message *COSESignMessage, publicKey *ecdsa.PublicKey, external []byte) (ok bool, err error) {
-	hashed, _, err := hashSigStructure(message, publicKey, external)
-	if err != nil {
-		return false, err
-	}
+// Verify
+// TODO: verify should return err and not ok
+func Verify(message *COSESignMessage, publicKey crypto.PublicKey, external []byte) (ok bool, err error) {
+	// fmt.Println(fmt.Sprintf("Verify Crv: %+v", publicKey.Curve.Params().BitSize))
+	// fmt.Println(fmt.Sprintf("pre Hash msg: %+v", message))
 
-	keySize := publicKey.Curve.Params().BitSize / 8
+	switch key := publicKey.(type) {
+	case *rsa.PublicKey:
+		// return false, errors.New("Unrecognized rsa publicKey type.")
+		hashed, _, err := hashSigStructure(message, key, external)
+		// fmt.Println(fmt.Sprintf("Hash err: %+v %+v", err, message))
+		if err != nil {
+			return false, err
+		}
 
-	// r and s from sig
-	signature := message.signatures[0].signature
-	if len(signature) != 2 * keySize {
-		return false, errors.New(fmt.Sprintf("invalid signature length: %d", len(signature)))
-	}
+		// keySize := key.Curve.Params().BitSize / 8
+		signature := message.signatures[0].signature
+		// if len(signature) != 2 * keySize {
+		// 	return false, errors.New(fmt.Sprintf("invalid signature length: %d", len(signature)))
+		// }
 
-	r := big.NewInt(0).SetBytes(signature[:keySize])
-	s := big.NewInt(0).SetBytes(signature[keySize:])
+		// TODO: pass signature alg or pick hash based on protected alg value
+		err = rsa.VerifyPSS(key, crypto.SHA256, hashed, signature, nil)
+		if err != nil {
+			// todo: wrap err?
+			return false, errors.New(fmt.Sprintf("verification failed %s", err))
+		} else {
+			return true, nil
+		}
+	case *ecdsa.PublicKey:
+		hashed, _, err := hashSigStructure(message, key, external)
+		// fmt.Println(fmt.Sprintf("Hash err: %+v %+v", err, message))
+		if err != nil {
+			return false, err
+		}
 
-	ok = ecdsa.Verify(publicKey, hashed, r, s)
-	if ok {
-		return ok, nil
-	} else {
-		return ok, errors.New("verification failed")
+		keySize := key.Curve.Params().BitSize / 8
+
+		// r and s from sig
+		signature := message.signatures[0].signature
+		if len(signature) != 2 * keySize {
+			return false, errors.New(fmt.Sprintf("invalid signature length: %d", len(signature)))
+		}
+
+		r := big.NewInt(0).SetBytes(signature[:keySize])
+		s := big.NewInt(0).SetBytes(signature[keySize:])
+
+		ok = ecdsa.Verify(key, hashed, r, s)
+		if ok {
+			return ok, nil
+		} else {
+			return ok, errors.New("verification failed")
+		}
+	default:
+		return false, errors.New("Unrecognized publicKey type.")
 	}
 }
